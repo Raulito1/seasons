986d0035f1547feec6e3d1b4b5494e9e014040a6
Raul <ralvaradojr399@gmail.com>
2022-05-08 19:09:20 -0400

    refactored functional components to class components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// component imports
+import SeasonDisplay from './SeasonDisplay';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class App extends React.Component {
+
+   constructor(props) {
+      super(props);
+
+      this.state = { lat: null };
+
+      window.navigator.geolocation.getCurrentPosition(
+         (position) => {
+            this.setState({ lat: position.coords.latitude });
+         },
+         (err) => console.log(err)
+      );
+   }
+   render() {
+      return (
+         <div>Latitude: {this.state.lat}</div>
+      )
+   }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
