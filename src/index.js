d91ff033b52795f58739cd4c2b1492df0dfa5c48
Raul <ralvaradojr399@gmail.com>
2022-05-08 19:16:01 -0400

    Added error message state

--- src/index.js
+++ src/index.js
@@ -9,19 +9,21 @@ class App extends React.Component {
    constructor(props) {
       super(props);
 
-      this.state = { lat: null };
+      this.state = { lat: null, errorMessage: '' };
 
       window.navigator.geolocation.getCurrentPosition(
          (position) => {
             this.setState({ lat: position.coords.latitude });
          },
-         (err) => console.log(err)
+         (err) => {
+            this.setState({ errorMessage: err.message });
+         }
       );
    }
    render() {
       return (
-         <div>Latitude: {this.state.lat}</div>
-      )
+         <div>Latitude: {this.state.lat} <br /> Error: {this.state.errorMessage}</div>
+      );
    }
 }
 
