30f6baf42c43eeafeab2160f392a769e40f0c594
Raul <ralvaradojr399@gmail.com>
2022-05-08 22:34:45 -0400

    Added season Conig object to refactor repeat code, added sematic ui css for icons

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,33 @@
 import React from 'react';
 
-const SeasonDisplay = () => {
-   return <div>Seasons</div>
+const seasonConfig = {
+   summer: {
+      text: "Let's hit the beach!",
+      iconName: 'sun'
+   },
+   winter: {
+      text: "Burr, it's chilly!",
+      iconName: 'snowflake'
+   }
 }
+
+const determineSeason = (lat, month) => {
+   if (month > 2 && month < 9) {
+      return lat > 0 ? 'summer' : 'winter';
+   } else {
+      return lat > 0 ? 'winter' : 'summer';
+   }
+}
+const SeasonDisplay = (props) => {
+   const season = determineSeason(props.lat, new Date().getMonth());
+   const { text, iconName } = seasonConfig[season];
+   return (
+      <div>
+         <i className={`${iconName} icon`} />
+         <h1>{text}</h1>
+         <i className={`${iconName} icon`} />
+      </div>
+   )
+};
 
 export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -4,13 +4,29 @@ import ReactDOM from 'react-dom';
 // component imports
 import SeasonDisplay from './SeasonDisplay';
 
+
+
 class App extends React.Component {
 
-   constructor(props) {
-      super(props);
+   state = {
+      lat: null,
+      errorMessage: ''
+   }
+   // constructor(props) {
+   //    super(props);
 
-      this.state = { lat: null, errorMessage: '' };
+   //    this.state = { lat: null, errorMessage: '' };
 
+   //    window.navigator.geolocation.getCurrentPosition(
+   //       (position) => {
+   //          this.setState({ lat: position.coords.latitude });
+   //       },
+   //       (err) => {
+   //          this.setState({ errorMessage: err.message });
+   //       }
+   //    );
+   // }
+   componentDidMount() {
       window.navigator.geolocation.getCurrentPosition(
          (position) => {
             this.setState({ lat: position.coords.latitude });
